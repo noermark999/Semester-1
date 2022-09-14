20c9436119d7dfb2df774043b346838a1a240fd3
BuildTools <unconfigured@null.spigotmc.org>
2022-09-14 22:05:35 +0200

    lek1409

--- Lek1409/src/Opgave5/Copier.java
+++ Lek1409/src/Opgave5/Copier.java
@@ -13,8 +13,9 @@ public class Copier {
     public void insertPaper(int paper) {
         if (this.paper + paper <=500) {
             this.paper = this.paper + paper;
+            System.out.println("You have inserted " + paper + " pieces of paper");
         } else if (this.paper + paper>500) {
-            System.out.println("Error cant insert more than 500");
+            System.out.println("Error you tried inserting " + paper + " pieces of paper, but copier can only hold 500");
         }
     }
     public int getPaper() {
@@ -25,20 +26,27 @@ public class Copier {
         if (!paperStuck) {
             if (this.paper-paper>=0) {
                 this.paper = this.paper - paper;
+                System.out.println("You have copied " + paper + " pieces of paper");
             } else {
-                System.out.println("Not enough paper");
+                System.out.println("You tried coping " + paper + " pieces of paper, but theres only " + this.paper + " pieces in the tray");
             }
         } else {
-            System.out.println("Error: Paper stuck");
+            System.out.println("Error: You tried copying " + paper + " pieces of paper but there's a paperjam!");
         }
     }
 
     public void makePaperJam() {
         paperStuck = true;
+        System.out.println("Paper has jammed!");
     }
 
     public void fixJam() {
         paperStuck = false;
+        System.out.println("The paperjam has been resolved");
+    }
+
+    public void printPaper() {
+        System.out.println("Pieces of paper in the tray: " + paper);
     }
 
 }
--- Lek1409/src/Opgave5/CopierApp.java
+++ Lek1409/src/Opgave5/CopierApp.java
@@ -3,17 +3,18 @@ package Opgave5;
 public class CopierApp {
     public static void main(String[] args) {
         Copier kopi = new Copier(19);
-        kopi.makePaperJam();                   //paper stuck
-        kopi.makeCopy(6);                //prøver at lave kopier, men der kommer error
-        System.out.println("Antal stk. papir i printer " + kopi.getPaper());
-        kopi.insertPaper(47);                   //indsætter 47 stk. papir uden problemer
-        System.out.println("Antal stk. papir i printer " + kopi.getPaper());
-        kopi.insertPaper(600);                  //prøver at indsætte mere end 500, error
-        System.out.println("Antal stk. papir i printer " + kopi.getPaper());
-        kopi.fixJam();                          //fixer papirstop
-        kopi.makeCopy(10);                //prøver at lave 10 kopier og virker
-        System.out.println("Antal stk. papir i printer " + kopi.getPaper());
-        kopi.makeCopy(100);               //prøver at lave flere kopier end der er papir, error
-        System.out.println("Antal stk. papir i printer " + kopi.getPaper());
+        kopi.printPaper();
+        kopi.makePaperJam();
+        kopi.makeCopy(6);
+        kopi.printPaper();
+        kopi.insertPaper(47);
+        kopi.printPaper();
+        kopi.insertPaper(600);
+        kopi.printPaper();
+        kopi.fixJam();
+        kopi.makeCopy(10);
+        kopi.printPaper();
+        kopi.makeCopy(100);
+        kopi.printPaper();
     }
 }
