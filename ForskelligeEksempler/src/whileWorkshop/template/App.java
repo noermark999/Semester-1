7e6047fa53fd7801df61e25396728321cd9baa31
BuildTools <unconfigured@null.spigotmc.org>
2022-09-17 12:38:06 +0200

    lek1609

---
+++ ForskelligeEksempler/src/whileWorkshop/template/App.java
@@ -0,0 +1,10 @@
+package whileWorkshop.template;
+
+import javafx.application.Application;
+
+public class App {
+
+	public static void main(String[] args) {
+		Application.launch(Gui.class);
+	}
+}
