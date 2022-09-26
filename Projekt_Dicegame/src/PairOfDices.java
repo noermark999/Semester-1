40f859cd4e0eb164389e055dd0f1f6df27979aaf
Noermark999 <jakobnoermark@hotmail.com>
2022-09-26 09:24:43 +0200

    lek9

---
+++ Projekt_Dicegame/src/PairOfDices.java
@@ -0,0 +1,25 @@
+/**
+ * This class models one pair of dices.
+ *
+ */
+public class PairOfDices {
+    /**
+     * The first die in the pair.
+     */
+	private Die die1;
+	/**
+	 * The second die in the pair.
+	 */
+	private Die die2;
+
+	/**
+	 * Constructor for objects of class PairOfDices
+	 */
+	// TODO: put the constructor here!
+
+	public PairOfDices() {
+		die1 = new Die();
+		die2 = new Die();
+		
+	}
+}
