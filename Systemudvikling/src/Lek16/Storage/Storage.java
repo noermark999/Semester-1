941c5c237598afcb56ae24743ee2c348abc6cedb
Noermark999 <jakobnoermark@hotmail.com>
2022-11-29 10:00:06 +0100

    Lek20

--- Systemudvikling/src/Lek16/Storage/Storage.java
+++ Systemudvikling/src/Lek16/Storage/Storage.java
@@ -1,41 +1,18 @@
 package Lek16.Storage;
 
-import Lek16.Application.Model.Car;
-import Lek16.Application.Model.CarType;
-import Lek16.Application.Model.Customer;
-import Lek16.Application.Model.Rent;
+import Lek16.Model.KontoType;
 
 import java.util.ArrayList;
 
+
 public class Storage {
 
-    private static final ArrayList<Car> cars = new ArrayList<>();
-    private static final ArrayList<Customer> customers = new ArrayList<>();
-    private static final ArrayList<Rent> rents = new ArrayList<>();
+	private static ArrayList<KontoType> kontoTyper = new ArrayList<KontoType>();
 
-    public static ArrayList<Customer> getCustomers() {
-        return new ArrayList<Customer>(customers);
-    }
-    public static void addCustomer(Customer customer) {
-        customers.add(customer);
-    }
-    public static void removeCustomer(Customer customer) {
-        customers.remove(customer);
-    }
+	public static void addKontoType(KontoType kontoType) {
+		if (!kontoTyper.contains(kontoType)) {
+			kontoTyper.add(kontoType);
+		}
+	}
 
-    public static ArrayList<Car> getCars() {
-        return new ArrayList<Car>(cars);
-    }
-
-    public static void addCar(Car car) {
-        cars.add(car);
-    }
-
-    public static ArrayList<Rent> getRents() {
-        return new ArrayList<Rent>(rents);
-    }
-
-    public static void addRent(Rent rent) {
-        rents.add(rent);
-    }
 }
