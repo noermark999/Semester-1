d0f94886599d11203013fc6130a6f31c98d02d31
Noermark999 <jakobnoermark@hotmail.com>
2023-01-16 19:44:24 +0100

    Lek20

--- Semesteropgaver/Semesteropgave_Juni_2021/src/test.java
+++ Semesteropgaver/Semesteropgave_Juni_2021/src/test.java
@@ -10,10 +10,10 @@ import java.util.Collections;
 public class test {
     public static void main(String[] args) {
         Kamp kamp1 = Controller.createKamp("Herning", LocalDate.of(2021,10,13), LocalTime.of(10,30));
-        Kamp kamp2 = Controller.createKamp("Viborg", LocalDate.of(2021,10,11), LocalTime.of(10,30));
-        Kamp kamp3 = Controller.createKamp("Harboøre", LocalDate.of(2021,10,12), LocalTime.of(10,30));
+        Kamp kamp2 = Controller.createKamp("Viborg", LocalDate.of(2021,10,12), LocalTime.of(10,30));
+        Kamp kamp3 = Controller.createKamp("Harboøre", LocalDate.of(2021,10,10), LocalTime.of(10,30));
         Kamp kamp4 = Controller.createKamp("Herning", LocalDate.of(2021,10,13), LocalTime.of(10,30));
-        Kamp kamp5 = Controller.createKamp("Thyborøn", LocalDate.of(2021,10,10), LocalTime.of(10,30));
+        Kamp kamp5 = Controller.createKamp("Thyborøn", LocalDate.of(2021,10,12), LocalTime.of(10,30));
 
         ArrayList<Kamp> kampe1 = new ArrayList<>();
         kampe1.add(kamp1);
